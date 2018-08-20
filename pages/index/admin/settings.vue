7c72963666efe8761208382cb5e60300e30f0400
Eduardo <du-du7@hotmail.com>
2018-08-20 17:16:41 -0300

    Alterando página settings

--- pages/index/admin/settings.vue
+++ pages/index/admin/settings.vue
@@ -3,7 +3,7 @@
         <v-card width="600">
             <v-container>
                 <v-card-title>
-                    <div class="display-1">Configurações</div>
+                    <div class="display-1 fill-width text-xs-center">Configurações</div>
                 </v-card-title>
                 <v-card-actions>
                     <v-layout row wrap justify-center>
@@ -16,37 +16,75 @@
                         <v-flex xs8>
                             <v-layout row align-center>
                                 <v-flex>
-                                    <div class="subheading" style="padding: 22px 16px 16px 0px;">Visitante:</div>
+                                    <div class="title">Visitante:</div>
                                 </v-flex>
-                                <v-flex>
-                                    <v-radio-group hide-details v-model="radios" row :mandatory="false">
-                                        <v-layout justify-space-around align-content-center>
-                                            <v-radio label="Ativado" value="value"></v-radio>
-                                            <v-radio label="Desativado" value="value1"></v-radio>                                            
-                                        </v-layout>
-                                    </v-radio-group>  
+                                <v-flex class="v-input-padding-top-0">
+                                    <v-switch color="secondary" hide-details v-model="value"></v-switch> 
                                 </v-flex>                              
                             </v-layout>
                         </v-flex>
-                        <v-container fluid>
-                            <v-layout row wrap>
-                                <v-flex xs5 pl-5>
-                                    <v-layout row wrap justify-center>
-                                        <div>Locais</div>
-                                        <v-combobox :items="items"></v-combobox>
-                                        <v-btn color="primary">Add</v-btn>
-                                    </v-layout>
+                            <v-layout mt-4 row wrap justify-center>
+                                <v-flex xs8>
+                                        <div class="v-toolbar__content-no-padding">
+                                        <v-toolbar flat color="white">
+                                            <v-toolbar-title>Locais</v-toolbar-title>
+                                            <v-spacer></v-spacer>
+                                            <v-dialog v-model="dialog" max-width="500px">
+                                            <v-btn slot="activator" color="secondary" dark class="mb-2">Novo Item</v-btn>
+                                            <v-card>
+                                                <v-card-title>
+                                                <span class="headline">{{ formTitle }}</span>
+                                                </v-card-title>
+                                    
+                                                <v-card-text>
+                                                <v-container grid-list-md>
+                                                    <v-layout wrap>
+                                                    <v-flex xs12 sm6 md4>
+                                                        <v-text-field v-model="editedItem.localName" label="Local"></v-text-field>
+                                                    </v-flex>
+                                                    </v-layout>
+                                                </v-container>
+                                                </v-card-text>
+                                    
+                                                <v-card-actions>
+                                                <v-spacer></v-spacer>
+                                                <v-btn color="blue darken-1" flat @click.native="close">Cancelar</v-btn>
+                                                <v-btn color="blue darken-1" flat @click.native="save">Salvar</v-btn>
+                                                </v-card-actions>
+                                            </v-card>
+                                            </v-dialog>
+                                        </v-toolbar>
+                                        <v-data-table :headers="headers" :items="locals" hide-actions class="elevation-1">
+                                            <template slot="items" slot-scope="props">
+                                            <td>{{ props.item.localName }}</td>
+                                            <td class="text-xs-right"></td>
+                                            <td class="justify-center layout px-0">
+                                                <v-icon
+                                                small
+                                                class="mr-2"
+                                                @click="editItem(props.item)"
+                                                >
+                                                edit
+                                                </v-icon>
+                                                <v-icon
+                                                small
+                                                @click="deleteItem(props.item)"
+                                                >
+                                                delete
+                                                </v-icon>
+                                            </td>
+                                            </template>
+                                            <template slot="no-data">
+                                            <v-btn color="primary" @click="initialize">Reset</v-btn>
+                                            </template>
+                                        </v-data-table>
+                                        </div>
                                 </v-flex>
-                                <v-spacer></v-spacer>
-                                <v-flex xs5 pr-5>
+                                <v-flex xs8 pr-5>
                                     <v-layout row wrap justify-center>
-                                        <div>Categorias</div>
-                                        <v-combobox :items="categorys"></v-combobox>
-                                        <v-btn color="primary">Add</v-btn>
                                     </v-layout>
                                 </v-flex>
                             </v-layout>   
-                        </v-container>
                     </v-layout>                    
                 </v-card-actions>
             </v-container>
@@ -56,26 +94,111 @@
 
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Watch } from "vue-property-decorator";
+import { setTimeout } from "timers";
 
 @Component
 export default class extends Vue {
-    private items: string[] = [
-        'Bloco A',
-        'Sala 321',
-        'Bloco C'
-    ]
 
-    private categorys: string[] = [
-        'Celular',
-        'Caderno',
-        'Identidade'
+    private dialog: boolean = false
+    private headers: any[] = [
+        {
+            text: 'Locais',
+            align: 'left',
+            value: 'localName'
+        },
+        {
+            text: '',
+            sortable: false
+        },
+        {
+            text: 'Ações',
+            value: 'localName',
+            align: 'center',
+            sortable: false
+        }
     ]
+    private locals: any[] = []
+    private editedIndex: number = -1
+    private editedItem: any = {
+        localName: ''
+    }
+    private defaultItem: any = {
+        localName: ''
+    }
+
+    private get formTitle () {
+      return this.editedIndex === -1 ? 'Novo Item' : 'Editar Item'
+    }
+
+    @Watch("dialog")
+    private onDialogChange (val : any){
+        val || this.close()
+    }
+
+    private created(){
+        this.initialize()
+        console.log('Bom Dia');
+        
+    }
+
+    private initialize() {
+        this.locals = [
+            {
+                localName: 'Bloca A'
+            },
+            {
+                localName: 'Bloco C'
+            },
+            {
+                localName: 'Sala 321'
+            }
+        ]
+    }
+
+    private editItem(item : any){
+        this.editedIndex = this.locals.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+    }
+
+    private deleteItem(item : any) {
+        const index : any = this.locals.indexOf(item)
+        confirm('Você tem certeza que deseja deletar este item?') && this.locals.splice(index, 1)
+    }
+
+    private close() {
+        this.dialog = false
+        setTimeout(() => {
+            this.editedItem = Object.assign({}, this.defaultItem)
+            this.editedIndex = -1
+        }, 300)
+    }
+
+    private save() {
+    if (this.editedIndex > -1) {
+        Object.assign(this.locals[this.editedIndex], this.editedItem)
+      } else {
+        this.locals.push(this.editedItem)
+      }
+      this.close()
+    }
 }
 </script>
 
+<style>
+.v-toolbar__content-no-padding .v-toolbar__content{
+    padding: 0 !important
+}
+</style>
+
 
 <style scoped>
+
+.v-input-padding-top-0 .v-input{
+    margin-top: 4px !important
+}
+
 .v-input--selection-controls{
   padding-top: 0 !important;
 }
