9f8e5cc67f08dff029fe1c8e55b61f14b77ebeff
Gabriel Mochi Ribeiro <gmochi56@icloud.com>
2018-08-21 07:53:38 -0300

    retrieve paga done

--- pages/index/retrieve/_id.vue
+++ pages/index/retrieve/_id.vue
@@ -1,8 +1,61 @@
 <template>
-    <v-container>
-        <h1>testando</h1>
-        <img :src="selectedItem.photoURL" alt="">
-    </v-container>
+  <v-container grid-list-xl>
+    <h1 class="mb-4">Concluir reivindicação</h1>
+    <v-stepper v-model="stepperModel" vertical>
+      <v-stepper-step step="1" :complete="stepperModel > 1">
+        {{ (stepperModel > 1) ? 'Item confirmado' : 'Confirmação' }}
+        <small>Confirme o item antes de se justificar.</small>
+      </v-stepper-step>
+      <v-stepper-content step="1">
+        <v-card flat>
+          <v-card-media contain :src="selectedItem.photoURL" height="300px">
+            <v-container fluid fill-height>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="title">{{ selectedItem.description }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-media>
+          <v-card-text>
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-icon>category</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title>{{ selectedItem.category.name }}</v-list-tile-title>
+              </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-icon>place</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title>{{ selectedItem.local.name }}</v-list-tile-title>
+              </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-icon>event</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-title>{{ selectedItem.datetime.toISOString() }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-card-text>
+          <v-card-actions class="px-4">
+            <v-btn color="secondary" @click="stepperModel = 2">Prosseguir</v-btn>
+            <v-btn flat>Cancelar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-stepper-content>
+      <v-stepper-step step="2">
+        Justificativa
+        <small>Elabore uma justificativa evidenciando os fatos que comprovam que esse item lhe pertence.</small>
+      </v-stepper-step>
+      <v-stepper-content step="2">
+        <v-text-field label="Justificativa" textarea class="mb-3"></v-text-field>
+        <v-btn color="secondary">Finalizar</v-btn>
+        <v-btn flat @click="stepperModel = 1">Voltar</v-btn>
+      </v-stepper-content>
+    </v-stepper>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -15,7 +68,7 @@ import User, { UserPermission } from "~/model/User";
 
 @Component
 export default class extends Vue {
-  private items: Item[] = [
+  private items = [
     new Item(
       0,
       "Um estojo azul de zíper prata.",
@@ -136,30 +189,22 @@ export default class extends Vue {
       new Category(0, "Borracha"),
       new Local(0, "Sala 304")
     )
-  ];
+  ]
 
-  $route: Route;
-  private selectedItem: Item = new Item(null, null, null, null, null, null);
-  
+  private selectedItem = new Item(null, null, null, null, null, null)
+  private stepperModel = 1
 
-  private validate({ params }: any) {
-      return (/^\d+$/.test(params.id) &&
-      Number(params.id) < 8 &&
-      Number(params.id) >= 0)
-  }
-
-  private async created() {
-    console.log(this.items.length);
-    this.selectedItem = await this.items.find(
+  private created () {
+    this.selectedItem = this.items.find(
       (item: Item) => item.id == this.$route.params.id
-    );  
+    )
   }
 }
 </script>
 
 <style scoped>
-img {
-  width: 192px;
+#item-img {
+  width: 384px;
   height: auto;
 }
 </style>
